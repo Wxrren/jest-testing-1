94412f777c316f43c73c264bda5a88f0a1831d9e
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2024-03-06 20:02:51 +0000

    ADD: newGame and showScore functions

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -5,5 +5,17 @@ let game = {
     choices: ["Button1", "Button2", "Button3", "Button4"],
 }
 
-module.exports = { game };
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore };
+
 
