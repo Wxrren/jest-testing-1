cd9750eb7b5028be097f2651c1a54cebd04d223a
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2024-03-07 13:03:57 +0000

    ADD: Functions for event listeners

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -3,6 +3,8 @@ let game = {
     playerMoves: [],
     score: 0,
     turnNumber: 0,
+    lastButton: "",
+    turnInProgress: false,
     choices: ["button1", "button2", "button3", "button4"]
 };
 
@@ -10,14 +12,17 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     game.score = 0;
-    game.turnNumber = 0;
+
     for (let circle of document.getElementsByClassName("circle")) {
         if (circle.getAttribute("data-listener") !== "true") {
             circle.addEventListener("click", (e) => {
-               let move = e.target.getAttribute("id");
-               lightsOn(move);
-               game.playerMoves.push(move);
-               playerTurn();
+                if (game.currentGame.length > 0 && !game.turnInProgress) {
+                    let move = e.target.getAttribute("id");
+                    game.lastButton = move;
+                    game.playerMoves.push(move);
+                    lightsOn(move);
+                    playerTurn();
+                }
             });
             circle.setAttribute("data-listener", "true");
         }
@@ -32,6 +37,19 @@ function addTurn() {
     showTurns();
 }
 
+function showTurns() {
+    game.turnInProgress = true;
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+            game.turnInProgress = false;
+        }
+    }, 800);
+}
+
 function lightsOn(circ) {
     document.getElementById(circ).classList.add("light");
     setTimeout(function () {
@@ -39,25 +57,10 @@ function lightsOn(circ) {
     }, 400);
 }
 
-function showScore() {
-    document.getElementById("score").innerText = game.score;
-}
-
-function showTurns() {
-    game.turnNumber = 0;
-    let turns = setInterval (() => {
-        lightsOn(game.currentGame[game.turnNumber]);
-        game.turnNumber++;
-        if (game.turnNumber >= game.currentGame.length) {
-            clearInterval (turns);
-        }
-    }, 800);
-}
-
 function playerTurn() {
     let i = game.playerMoves.length - 1;
     if (game.currentGame[i] === game.playerMoves[i]) {
-        if (game.currentGame.length == game.playerMoves.length) {
+        if (game.currentGame.length === game.playerMoves.length) {
             game.score++;
             showScore();
             addTurn();
@@ -66,6 +69,10 @@ function playerTurn() {
         alert("Wrong move!");
         newGame();
     }
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
 }
 
 module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
